e6bc0ed97b36c6fc1c553fa0a1dde0a71dc34cfb
Louis Salkeld <louissalkeld@outlook.com>
2020-09-05 23:36:18 +0100

    Implement AudioAnalysis sub-objects

--- Spotify/ObjectModel/Section.cs
+++ Spotify/ObjectModel/Section.cs
@@ -4,9 +4,40 @@ namespace Spotify.ObjectModel
 {
     public record Section : TimeInterval
     {
-        internal Section(Single start, Single duration, Single confidence) : base(start, duration, confidence)
+        internal Section(
+            Single start,
+            Single duration,
+            Single confidence,
+            Single loudness,
+            Single tempo,
+            Single tempoConfidence,
+            Int32 key,
+            Single keyConfidence,
+            Modality? mode,
+            Single modeConfidence,
+            Int32 timeSignature,
+            Single timeSignatureConfidence) :
+            base(start, duration, confidence)
         {
-
+            this.Loudness = loudness;
+            this.Tempo = tempo;
+            this.TempoConfidence = tempoConfidence;
+            this.Key = key;
+            this.KeyConfidence = keyConfidence;
+            this.Mode = mode;
+            this.ModeConfidence = modeConfidence;
+            this.TimeSignature = timeSignature;
+            this.TimeSignatureConfidence = timeSignatureConfidence;
         }
+
+        public Single Loudness { get; init; }
+        public Single Tempo { get; init; }
+        public Single TempoConfidence { get; init; }
+        public Int32 Key { get; init; }
+        public Single KeyConfidence { get; init; }
+        public Modality? Mode { get; init; }
+        public Single ModeConfidence { get; init; }
+        public Int32 TimeSignature { get; init; }
+        public Single TimeSignatureConfidence { get; init; }
     }
 }
--- Spotify/ObjectModel/Segment.cs
+++ Spotify/ObjectModel/Segment.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Spotify.ObjectModel
 {
     public record Segment : TimeInterval
     {
-        internal Segment(Single start, Single duration, Single confidence) : base(start, duration, confidence)
+        internal Segment(
+            Single start,
+            Single duration,
+            Single confidence,
+            Single loudnessStart,
+            Single loudnessEnd,
+            Single loudnessMax,
+            Single loudnessMaxTime,
+            IReadOnlyList<Single> pitches,
+            IReadOnlyList<Single> timbre) :
+            base(start, duration, confidence)
         {
-
+            this.LoudnessStart = loudnessStart;
+            this.LoudnessEnd = loudnessEnd;
+            this.LoudnessMax = loudnessMax;
+            this.LoudnessMaxTime = loudnessMaxTime;
+            this.Pitches = pitches;
+            this.Timbre = timbre;
         }
+
+        public Single LoudnessStart { get; init; }
+        public Single LoudnessEnd { get; init; }
+        public Single LoudnessMax { get; init; }
+        public Single LoudnessMaxTime { get; init; }
+        public IReadOnlyList<Single> Pitches { get; init; }
+        public IReadOnlyList<Single> Timbre { get; init; }
     }
 }
