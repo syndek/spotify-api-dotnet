7c4ddc9b3ca8e4a859d9e559a0eaaa42d9390f92
Louis Salkeld <louissalkeld@outlook.com>
2020-09-08 03:22:56 +0100

    Document AlbumType & AlbumGroups

--- Spotify/ObjectModel/AlbumGroups.cs
+++ Spotify/ObjectModel/AlbumGroups.cs
@@ -2,12 +2,27 @@
 
 namespace Spotify.ObjectModel
 {
+    /// <summary>
+    /// Defines different relationships between an <see cref="Artist"/> and an <see cref="ObjectModel.Album"/>.
+    /// </summary>
     [Flags]
     public enum AlbumGroups : Int32
     {
+        /// <summary>
+        /// The <see cref="ObjectModel.Album"/> is an album by the <see cref="Artist"/>.
+        /// </summary>
         Album = 0x01,
+        /// <summary>
+        /// The <see cref="ObjectModel.Album"/> is a single by the <see cref="Artist"/>.
+        /// </summary>
         Single = 0x02,
+        /// <summary>
+        /// The <see cref="ObjectModel.Album"/> is a compilation by the <see cref="Artist"/>.
+        /// </summary>
         Compilation = 0x04,
+        /// <summary>
+        /// The <see cref="Artist"/> appears on the <see cref="ObjectModel.Album"/>, but it is not by them.
+        /// </summary>
         AppearsOn = 0x08
     }
 }
--- Spotify/ObjectModel/AlbumType.cs
+++ Spotify/ObjectModel/AlbumType.cs
@@ -2,10 +2,22 @@
 
 namespace Spotify.ObjectModel
 {
+    /// <summary>
+    /// Defines different types of <see cref="ObjectModel.Album"/>.
+    /// </summary>
     public enum AlbumType : Int32
     {
+        /// <summary>
+        /// The <see cref="ObjectModel.Album"/> is a regular album.
+        /// </summary>
         Album,
+        /// <summary>
+        /// The <see cref="ObjectModel.Album"/> is a single.
+        /// </summary>
         Single,
+        /// <summary>
+        /// The <see cref="ObjectModel.Album"/> is a compilation of tracks from other albums.
+        /// </summary>
         Compilation
     }
 }
