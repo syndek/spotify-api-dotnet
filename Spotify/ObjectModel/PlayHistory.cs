cb4f1995bdbfe8dd32bde6d700298bb6e0e549e9
Louis Salkeld <louissalkeld@outlook.com>
2020-09-08 03:45:09 +0100

    Modify PlayHistory property order

--- Spotify/ObjectModel/PlayHistory.cs
+++ Spotify/ObjectModel/PlayHistory.cs
@@ -11,13 +11,13 @@ namespace Spotify.ObjectModel
         /// Initializes a new instance of the <see cref="PlayHistory"/> record with the specified values.
         /// </summary>
         /// <param name="track">A <see cref="SimplifiedTrack"/> representing the track the user listened to.</param>
-        /// <param name="playedAt">The date and time the track was played.</param>
         /// <param name="context">The <see cref="ObjectModel.Context"/> the track was played from.</param>
-        internal PlayHistory(SimplifiedTrack track, DateTime playedAt, Context context) : base()
+        /// <param name="playedAt">The date and time the track was played.</param>
+        internal PlayHistory(SimplifiedTrack track, Context context, DateTime playedAt) : base()
         {
             this.Track = track;
-            this.PlayedAt = playedAt;
             this.Context = context;
+            this.PlayedAt = playedAt;
         }
 
         /// <summary>
@@ -26,14 +26,14 @@ namespace Spotify.ObjectModel
         /// <returns>A <see cref="SimplifiedTrack"> representing the track the user listened to.</returns>
         public SimplifiedTrack Track { get; init; }
         /// <summary>
-        /// Gets or sets the date and time the track was played.
-        /// </summary>
-        /// <returns>The date and time the track was played.</returns>
-        public DateTime PlayedAt { get; init; }
-        /// <summary>
         /// Gets or sets the <see cref="ObjectModel.Context"/> the track was played from.
         /// </summary>
         /// <returns>The <see cref="ObjectModel.Context"/> the track was played from.</returns>
         public Context Context { get; init; }
+        /// <summary>
+        /// Gets or sets the date and time the track was played.
+        /// </summary>
+        /// <returns>The date and time the track was played.</returns>
+        public DateTime PlayedAt { get; init; }
     }
 }
