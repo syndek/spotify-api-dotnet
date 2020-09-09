41b0be87ea8a892a712834549465de73d42174ea
Louis Salkeld <louissalkeld@outlook.com>
2020-09-09 23:52:31 +0100

    Implement ContextConverter

--- Spotify/ObjectModel/JsonConverters/ContextConverter.cs
+++ Spotify/ObjectModel/JsonConverters/ContextConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -12,7 +13,43 @@ namespace Spotify.ObjectModel.JsonConverters
 
         public override Context Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            throw new NotImplementedException();
+            reader.AssertTokenType(JsonTokenType.StartObject);
+
+            Uri uri = null!;
+            Uri href = null!;
+            IReadOnlyDictionary<String, Uri> externalUrls = null!;
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.EndObject)
+                {
+                    break;
+                }
+
+                if (reader.TokenType != JsonTokenType.PropertyName)
+                {
+                    throw new JsonException();
+                }
+
+                switch (reader.GetString())
+                {
+                    case "uri":
+                        uri = reader.ReadUri();
+                        break;
+                    case "href":
+                        href = reader.ReadUri();
+                        break;
+                    case "external_urls":
+                        reader.Read(JsonTokenType.StartObject);
+                        externalUrls = reader.ReadExternalUrls();
+                        break;
+                    default:
+                        reader.Skip();
+                        break;
+                }
+            }
+
+            return new(uri, href, externalUrls);
         }
 
         public override void Write(Utf8JsonWriter writer, Context value, JsonSerializerOptions options) => throw new NotSupportedException();
