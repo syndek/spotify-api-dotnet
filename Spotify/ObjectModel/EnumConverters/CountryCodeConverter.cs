979b291ea1b372afc0d0604598911f334fda9574
Louis Salkeld <louissalkeld@outlook.com>
2020-09-13 03:13:01 +0100

    Remove unnecessary null check & fix accessibility modifiers in CountryCodeConverter

--- Spotify/ObjectModel/EnumConverters/CountryCodeConverter.cs
+++ Spotify/ObjectModel/EnumConverters/CountryCodeConverter.cs
@@ -4,18 +4,10 @@ namespace Spotify.ObjectModel.EnumConverters
 {
     internal static class CountryCodeConverter : Object
     {
-        public static CountryCode FromSpotifyString(String countryCode) =>
+        internal static CountryCode FromSpotifyString(String countryCode) =>
             (countryCode == "from_token") ? CountryCode.FromToken : Enum.Parse<CountryCode>(countryCode);
 
-        public static String ToSpotifyString(this CountryCode countryCode)
-        {
-            if (countryCode == CountryCode.FromToken)
-            {
-                return "from_token";
-            }
-
-            return countryCode.GetName().ToLower() ??
-                throw new ArgumentException($"Invalid {nameof(CountryCode)} value: {countryCode}", nameof(countryCode));
-        }
+        internal static String ToSpotifyString(this CountryCode countryCode) =>
+            (countryCode == CountryCode.FromToken) ? "from_token" : countryCode.GetName().ToLower();
     }
 }
