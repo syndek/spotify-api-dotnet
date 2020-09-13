c1b6f6e496bae551f10772a66cab6afce20435b5
Louis Salkeld <louissalkeld@outlook.com>
2020-09-13 02:33:07 +0100

    Document Product

--- Spotify/ObjectModel/Product.cs
+++ Spotify/ObjectModel/Product.cs
@@ -2,9 +2,18 @@
 
 namespace Spotify.ObjectModel
 {
+    /// <summary>
+    /// Defines different Spotify subscription levels.
+    /// </summary>
     public enum Product : Int32
     {
+        /// <summary>
+        /// The Spotify Free plan (can be considered the same as 'open').
+        /// </summary>
         Free,
+        /// <summary>
+        /// The Spotify Premium (paid) plan.
+        /// </summary>
         Premium
     }
 }
