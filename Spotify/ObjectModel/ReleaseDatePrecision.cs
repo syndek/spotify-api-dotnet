22cca6761865f15e238c9cca7d4a5ad0b6538baf
Louis Salkeld <louissalkeld@outlook.com>
2020-09-13 02:34:49 +0100

    Document ReleaseDatePrecision

--- Spotify/ObjectModel/ReleaseDatePrecision.cs
+++ Spotify/ObjectModel/ReleaseDatePrecision.cs
@@ -2,10 +2,22 @@
 
 namespace Spotify.ObjectModel
 {
+    /// <summary>
+    /// Defines the precisions with which a release date value can be known.
+    /// </summary>
     public enum ReleaseDatePrecision : Int32
     {
+        /// <summary>
+        /// Only the year of the release date is known.
+        /// </summary>
         Year,
+        /// <summary>
+        /// Only the month and year of the release date are known.
+        /// </summary>
         Month,
+        /// <summary>
+        /// The day, month, and year of the release date are all known.
+        /// </summary>
         Day
     }
 }
