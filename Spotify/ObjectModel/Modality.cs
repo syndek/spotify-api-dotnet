1201561295245593d781cd343e7eaa5b5618fcc4
Louis Salkeld <louissalkeld@outlook.com>
2020-09-13 02:31:15 +0100

    Document Modality

--- Spotify/ObjectModel/Modality.cs
+++ Spotify/ObjectModel/Modality.cs
@@ -2,9 +2,18 @@
 
 namespace Spotify.ObjectModel
 {
+    /// <summary>
+    /// Defines the different modalities of a track (the type of scale from which its melodic content is derived).
+    /// </summary>
     public enum Modality : Int32
     {
+        /// <summary>
+        /// The minor mode.
+        /// </summary>
         Minor,
+        /// <summary>
+        /// The major mode.
+        /// </summary>
         Major
     }
 }
