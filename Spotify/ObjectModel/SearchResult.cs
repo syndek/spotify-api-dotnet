fa9f0a0e2b17321de91127c0013f20f35d6a0339
Louis Salkeld <louissalkeld@outlook.com>
2020-09-13 02:59:04 +0100

    Document SearchResult & SearchResultTypes

--- Spotify/ObjectModel/SearchResult.cs
+++ Spotify/ObjectModel/SearchResult.cs
@@ -1,7 +1,31 @@
 ﻿namespace Spotify.ObjectModel
 {
+    /// <summary>
+    /// Represents the results of a call to <see cref="ISpotifySearchApi.SearchAsync"/>.
+    /// </summary>
     public record SearchResult : SpotifyObject
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SearchResult"/> record with the specified values.
+        /// </summary>
+        /// <param name="artists">
+        /// A <see cref="Paging{TItem}"/> of returned <see cref="Artist"/> objects, or <see langword="null"/> if not included.
+        /// </param>
+        /// <param name="albums">
+        /// A <see cref="Paging{TItem}"/> of returned <see cref="SimplifiedAlbum"/> objects, or <see langword="null"/> if not included.
+        /// </param>
+        /// <param name="tracks">
+        /// A <see cref="Paging{TItem}"/> of returned <see cref="Track"/> objects, or <see langword="null"/> if not included.
+        /// </param>
+        /// <param name="shows">
+        /// A <see cref="Paging{TItem}"/> of returned <see cref="SimplifiedShow"/> objects, or <see langword="null"/> if not included.
+        /// </param>
+        /// <param name="episodes">
+        /// A <see cref="Paging{TItem}"/> of returned <see cref="SimplifiedEpisode"/> objects, or <see langword="null"/> if not included.
+        /// </param>
+        /// <param name="playlists">
+        /// A <see cref="Paging{TItem}"/> of returned <see cref="Playlist"/> objects, or <see langword="null"/> if not included.
+        /// </param>
         internal SearchResult(
             Paging<Artist>? artists,
             Paging<SimplifiedAlbum>? albums,
@@ -18,11 +42,47 @@
             this.Playlists = playlists;
         }
 
+        /// <summary>
+        /// Gets or sets the returned artists of the <see cref="SearchResult"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Paging{TItem}"/> of returned <see cref="Artist"/> objects, or <see langword="null"/> if not included.
+        /// </returns>
         public Paging<Artist>? Artists { get; init; }
+        /// <summary>
+        /// Gets or sets the returned albums of the <see cref="SearchResult"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Paging{TItem}"/> of returned <see cref="SimplifiedAlbum"/> objects, or <see langword="null"/> if not included.
+        /// </returns>
         public Paging<SimplifiedAlbum>? Albums { get; init; }
+        /// <summary>
+        /// Gets or sets the returned tracks of the <see cref="SearchResult"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Paging{TItem}"/> of returned <see cref="Track"/> objects, or <see langword="null"/> if not included.
+        /// </returns>
         public Paging<Track>? Tracks { get; init; }
+        /// <summary>
+        /// Gets or sets the returned shows of the <see cref="SearchResult"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Paging{TItem}"/> of returned <see cref="SimplifiedShow"/> objects, or <see langword="null"/> if not included.
+        /// </returns>
         public Paging<SimplifiedShow>? Shows { get; init; }
+        /// <summary>
+        /// Gets or sets the returned episodes of the <see cref="SearchResult"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Paging{TItem}"/> of returned <see cref="SimplifiedEpisode"/> objects, or <see langword="null"/> if not included.
+        /// </returns>
         public Paging<SimplifiedEpisode>? Episodes { get; init; }
+        /// <summary>
+        /// Gets or sets the returned playlists of the <see cref="SearchResult"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Paging{TItem}"/> of returned <see cref="Playlist"/> objects, or <see langword="null"/> if not included.
+        /// </returns>
         public Paging<Playlist>? Playlists { get; init; }
     }
 }
--- Spotify/ObjectModel/SearchResultTypes.cs
+++ Spotify/ObjectModel/SearchResultTypes.cs
@@ -2,14 +2,35 @@
 
 namespace Spotify.ObjectModel
 {
+    /// <summary>
+    /// Defines types that should be included in the result of a call to <see cref="ISpotifySearchApi.SearchAsync"/>.
+    /// </summary>
     [Flags]
     public enum SearchResultTypes : Int32
     {
+        /// <summary>
+        /// Albums should be included in the <see cref="SearchResult"/>.
+        /// </summary>
         Album = 0x01,
+        /// <summary>
+        /// Artists should be included in the <see cref="SearchResult"/>.
+        /// </summary>
         Artist = 0x02,
+        /// <summary>
+        /// Playlists should be included in the <see cref="SearchResult"/>.
+        /// </summary>
         Playlist = 0x04,
+        /// <summary>
+        /// Tracks should be included in the <see cref="SearchResult"/>.
+        /// </summary>
         Track = 0x08,
+        /// <summary>
+        /// Shows should be included in the <see cref="SearchResult"/>.
+        /// </summary>
         Show = 0x10,
+        /// <summary>
+        /// Episodes should be included in the <see cref="SearchResult"/>.
+        /// </summary>
         Episode = 0x20
     }
 }
