48c5d933fb899c4eeae99e17b23c542dcccaa874
Louis Salkeld <louissalkeld@outlook.com>
2020-09-13 02:16:08 +0100

    Document TimeRange

--- Spotify/ObjectModel/TimeRange.cs
+++ Spotify/ObjectModel/TimeRange.cs
@@ -2,10 +2,22 @@
 
 namespace Spotify.ObjectModel
 {
+    /// <summary>
+    /// Defines different ranges of time recognised by the Spotify Web API.
+    /// </summary>
     public enum TimeRange : Int32
     {
+        /// <summary>
+        /// Approximately the last 4 weeks.
+        /// </summary>
         ShortTerm,
+        /// <summary>
+        /// Approximately the last 6 months.
+        /// </summary>
         MediumTerm,
+        /// <summary>
+        /// Several years, including new data as it becomes available.
+        /// </summary>
         LongTerm
     }
 }
