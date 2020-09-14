a8af1152c69aef2149515c16ca231191bc4f8bbe
Louis Salkeld <louissalkeld@outlook.com>
2020-09-14 20:46:38 +0100

    Document Artist

--- Spotify/ObjectModel/Artist.cs
+++ Spotify/ObjectModel/Artist.cs
@@ -8,6 +8,26 @@ namespace Spotify.ObjectModel
     /// </summary>
     public record Artist : SimplifiedArtist
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Artist"/> record with the specified values.
+        /// </summary>
+        /// <param name="id">A <see cref="String"/> representing the Spotify ID of the artist.</param>
+        /// <param name="uri">The Spotify URI of the artist.</param>
+        /// <param name="href">A link to the Spotify Web API endpoint providing full details of the artist.</param>
+        /// <param name="name">A <see cref="String"/> representing the name of the artist.</param>
+        /// <param name="images">
+        /// A <see cref="IReadOnlyList{T}"/> of <see cref="Image"/> objects
+        /// representing images of the artist in various sizes.
+        /// </param>
+        /// <param name="followers">The <see cref="ObjectModel.Followers"/> of the artist.</param>
+        /// <param name="genres">
+        /// A <see cref="IReadOnlyList{T}"/> of <see cref="String"/> objects representing genres the artist is associated with.
+        /// </param>
+        /// <param name="popularity">An <see cref="Int32"/> representing the popularity of the artist.</param>
+        /// <param name="externalUrls">
+        /// A <see cref="IReadOnlyDictionary{TKey, TValue}"/> containing the known
+        /// external URLs for the artist, keyed by the type of the URL.
+        /// </param>
         internal Artist(
             String id,
             Uri uri,
@@ -26,9 +46,31 @@ namespace Spotify.ObjectModel
             this.Popularity = popularity;
         }
 
+        /// <summary>
+        /// Gets or sets the images of the <see cref="Artist"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="IReadOnlyList{T}"/> of <see cref="Image"/> objects
+        /// representing images of the <see cref="Artist"/> in various sizes.
+        /// </returns>
         public IReadOnlyList<Image> Images { get; init; }
+        /// <summary>
+        /// Gets or sets the <see cref="ObjectModel.Followers"/> of the <see cref="Artist"/>.
+        /// </summary>
+        /// <returns>The <see cref="ObjectModel.Followers"/> of the <see cref="Artist"/>.</returns>
         public Followers Followers { get; init; }
+        /// <summary>
+        /// Gets or sets the genres associated with the <see cref="Artist"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="IReadOnlyList{T}"/> of <see cref="String"/> objects representing genres the <see cref="Artist"/> is associated with.
+        /// </returns>
         public IReadOnlyList<String> Genres { get; init; }
+        /// <summary>        
+        /// Gets or sets the popularity of the <see cref="Artist"/>.
+        /// Popularity values will be between <c>0</c> and <c>100</c>, with <c>100</c> being the most popular.
+        /// </summary>
+        /// <returns>An <see cref="Int32"/> representing the popularity of the <see cref="Artist"/>.</returns>
         public Int32 Popularity { get; init; }
     }
 }
