1cc06248c7b4dd49f9a8479fcb7e84453d9ff070
Louis Salkeld <louissalkeld@outlook.com>
2020-09-14 03:20:55 +0100

    Document PublicUser

--- Spotify/ObjectModel/PublicUser.cs
+++ Spotify/ObjectModel/PublicUser.cs
@@ -3,8 +3,30 @@ using System.Collections.Generic;
 
 namespace Spotify.ObjectModel
 {
+    /// <summary>
+    /// Represents a Spotify user's public profile.
+    /// </summary>
     public record PublicUser : LocatableObject
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PublicUser"/> record with the specified values.
+        /// </summary>
+        /// <param name="id">A <see cref="String"/> representing the Spotify ID of the user.</param>
+        /// <param name="uri">The Spotify URI for the user.</param>
+        /// <param name="href">A link to the Spotify Web API endpoint providing full details of the user.</param>
+        /// <param name="displayName">
+        /// A <see cref="String"/> representing the display name of the user,
+        /// or <see langword="null"/> if not available.
+        /// </param>
+        /// <param name="images">
+        /// A <see cref="IReadOnlyList{T}"/> of <see cref="Image"/> objects
+        /// representing the profile image of the user in various sizes.
+        /// </param>
+        /// <param name="followers">The <see cref="ObjectModel.Followers"/> of the user.</param>
+        /// <param name="externalUrls">
+        /// A <see cref="IReadOnlyDictionary{TKey, TValue}"/> containing the known
+        /// external URLs for the user, keyed by the type of the URL.
+        /// </param>
         internal PublicUser(
             String id,
             Uri uri,
@@ -22,10 +44,39 @@ namespace Spotify.ObjectModel
             this.ExternalUrls = externalUrls;
         }
 
+        /// <summary>
+        /// Gets or sets a link to the Spotify Web API endpoint providing full details of <see cref="PublicUser"/>.
+        /// </summary>
+        /// <returns>A link to the Spotify Web API endpoint providing full details of the <see cref="PublicUser"/>.</returns>
         public Uri Href { get; init; }
+        /// <summary>
+        /// Gets or sets the display name of the <see cref="PublicUser"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="String"/> representing the display name of the <see cref="PublicUser"/>,
+        /// or <see langword="null"/> if not available.
+        /// </returns>
         public String? DisplayName { get; init; }
+        /// <summary>
+        /// Gets or sets the profile image of the <see cref="PublicUser"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="IReadOnlyList{T}"/> of <see cref="Image"/> objects
+        /// representing the profile image of the <see cref="PublicUser"/> in various sizes.
+        /// </returns>
         public IReadOnlyList<Image> Images { get; init; }
+        /// <summary>
+        /// Gets or sets the <see cref="ObjectModel.Followers"/> of the <see cref="PublicUser"/>.
+        /// </summary>
+        /// <returns>The <see cref="ObjectModel.Followers"/> of the <see cref="PublicUser"/>.</returns>
         public Followers Followers { get; init; }
+        /// <summary>
+        /// Gets or sets the known external URLs for the <see cref="PublicUser"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="IReadOnlyDictionary{TKey, TValue}"/> containing the known
+        /// external URLs for the <see cref="PublicUser"/>, keyed by the type of the URL.
+        /// </returns>
         public IReadOnlyDictionary<String, Uri> ExternalUrls { get; init; }
     }
 }
