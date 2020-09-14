fbb3f333b9baaebcce8ba1fdfcab55760c3241f2
Louis Salkeld <louissalkeld@outlook.com>
2020-09-14 04:16:44 +0100

    Document PrivateUser

--- Spotify/ObjectModel/PrivateUser.cs
+++ Spotify/ObjectModel/PrivateUser.cs
@@ -3,8 +3,42 @@ using System.Collections.Generic;
 
 namespace Spotify.ObjectModel
 {
+    /// <summary>
+    /// Represents a Spotify user's private profile.
+    /// </summary>
     public record PrivateUser : PublicUser
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PrivateUser"/> record with the specified values.
+        /// </summary>
+        /// <param name="id">A <see cref="String"/> representing the Spotify ID of the user.</param>
+        /// <param name="uri">The Spotify URI for the user.</param>
+        /// <param name="href">A link to the Spotify Web API endpoint providing full details of the user.</param>
+        /// <param name="email">
+        /// A <see cref="String"/> representing the email address of the user,
+        /// or <see langword="null"/> if not provided.
+        /// </param>
+        /// <param name="displayName">
+        /// A <see cref="String"/> representing the display name of the user,
+        /// or <see langword="null"/> if not available.
+        /// </param>
+        /// <param name="country">
+        /// A <see cref="CountryCode"/> representing the country of the user, as set in their profile,
+        /// or <see langword="null"/> if not provided.
+        /// </param>
+        /// <param name="images">
+        /// A <see cref="IReadOnlyList{T}"/> of <see cref="Image"/> objects
+        /// representing the profile image of the user in various sizes.
+        /// </param>
+        /// <param name="product">
+        /// The Spotify <see cref="ObjectModel.Product"/> of the user,
+        /// or <see langword="null"/> if not provided.
+        /// </param>
+        /// <param name="followers">The <see cref="ObjectModel.Followers"/> of the user.</param>
+        /// <param name="externalUrls">
+        /// A <see cref="IReadOnlyDictionary{TKey, TValue}"/> containing the known
+        /// external URLs for the user, keyed by the type of the URL.
+        /// </param>
         internal PrivateUser(
             String id,
             Uri uri,
@@ -23,8 +57,29 @@ namespace Spotify.ObjectModel
             this.Product = product;
         }
 
+        /// <summary>
+        /// Gets or sets the email address of the <see cref="PrivateUser"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="String"/> representing the email address of the <see cref="PrivateUser"/>,
+        /// or <see langword="null"/> if not provided.
+        /// </returns>
         public String? Email { get; init; }
+        /// <summary>
+        /// Gets or sets the country of the <see cref="PrivateUser"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="CountryCode"/> representing the country of the <see cref="PrivateUser"/>, as set in their profile,
+        /// or <see langword="null"/> if not provided.
+        /// </returns>
         public CountryCode? Country { get; init; }
+        /// <summary>
+        /// Gets or sets the <see cref="ObjectModel.Product"/> of the <see cref="PrivateUser"/>.
+        /// </summary>
+        /// <returns>
+        /// The Spotify <see cref="ObjectModel.Product"/> of the <see cref="PrivateUser"/>,
+        /// or <see langword="null"/> if not provided.
+        /// </returns>
         public Product? Product { get; init; }
     }
 }
