a818995e3bc75de5d4593d4630ed4f1bba96e5d4
Louis Salkeld <louissalkeld@outlook.com>
2020-09-17 23:38:19 +0100

    Document Section & Segment

--- Spotify.ObjectModel/Section.cs
+++ Spotify.ObjectModel/Section.cs
@@ -2,8 +2,39 @@
 
 namespace Spotify.ObjectModel
 {
+    /// <summary>
+    /// Represents a section of an <see cref="AudioAnalysis"/> object.
+    /// A section is a subdivision of a <see cref="Track"/> that contains a roughly consistent rhythm/timbre throughout its duration.
+    /// </summary>
     public record Section : TimeInterval
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Section"/> record with the specified values.
+        /// </summary>
+        /// <param name="start">A <see cref="Single"/> representing the starting point (in seconds) of the section.</param>
+        /// <param name="duration">A <see cref="Single"/> representing the duration (in seconds) of the section.</param>
+        /// <param name="confidence">
+        /// A <see cref="Single"/> representing the confidence of the reliability of the section's 'designation'.
+        /// </param>
+        /// <param name="loudness">A <see cref="Single"/> representing the overall loudness (in decibels) of the section.</param>
+        /// <param name="tempo">A <see cref="Single"/> representing the overall tempo (in beats per minute) of the section.</param>
+        /// <param name="tempoConfidence">
+        /// A <see cref="Single"/> representing the confidence of the reliability of the <paramref name="tempo"/> value.
+        /// </param>
+        /// <param name="key">An <see cref="Int32"/> representing the estimated overall key of the section.</param>
+        /// <param name="keyConfidence">
+        /// A <see cref="Single"/> representing the confidence of the reliability of the <paramref name="key"/> value.
+        /// </param>
+        /// <param name="mode">
+        /// A <see cref="Modality"/> value representing the modality of the section, or <see langword="null"/> if none is provided.
+        /// </param>
+        /// <param name="modeConfidence">
+        /// A <see cref="Single"/> representing the confidence of the reliability of the <paramref name="mode"/> value.
+        /// </param>
+        /// <param name="timeSignature">An <see cref="Int32"/> representing the estimated overall time signature of the section.</param>
+        /// <param name="timeSignatureConfidence">
+        /// A <see cref="Single"/> representing the confidence of the reliability of the <paramref name="timeSignature"/> value.
+        /// </param>
         public Section(
             Single start,
             Single duration,
@@ -30,14 +61,57 @@ namespace Spotify.ObjectModel
             this.TimeSignatureConfidence = timeSignatureConfidence;
         }
 
+        /// <summary>
+        /// Gets or sets the overall loudness (in decibels) of the <see cref="Section"/>.
+        /// </summary>
+        /// <returns>A <see cref="Single"/> representing the overall loudness (in decibels) of the <see cref="Section"/>.</returns>
         public Single Loudness { get; init; }
+        /// <summary>
+        /// Gets or sets the overall tempo (in beats per minute) of the <see cref="Section"/>.
+        /// </summary>
+        /// <returns>A <see cref="Single"/> representing the overall tempo (in beats per minute) of the <see cref="Section"/>.</returns>
         public Single Tempo { get; init; }
+        /// <summary>
+        /// Gets or sets the confidence of the reliability of the <see cref="Tempo"/> value, from <c>0.0</c> to <c>1.0</c>.
+        /// </summary>
+        /// <remarks><c>0.0</c> represents low confidence in the value, while <c>1.0</c> represents high confidence.</remarks>
+        /// <returns>A <see cref="Single"/> representing the confidence of the reliability of the <see cref="Tempo"/> value.</returns>
         public Single TempoConfidence { get; init; }
+        /// <summary>
+        /// Gets or sets the estimated overall key of the <see cref="Section"/>.
+        /// </summary>
+        /// <returns>An <see cref="Int32"/> representing the estimated overall key of the <see cref="Section"/>.</returns>
         public Int32 Key { get; init; }
+        /// <summary>
+        /// Gets or sets the confidence of the reliability of the <see cref="Key"/> value, from <c>0.0</c> to <c>1.0</c>.
+        /// </summary>
+        /// <remarks><c>0.0</c> represents low confidence in the value, while <c>1.0</c> represents high confidence.</remarks>
+        /// <returns>A <see cref="Single"/> representing the confidence of the reliability of the <see cref="Key"/> value.</returns>
         public Single KeyConfidence { get; init; }
+        /// <summary>
+        /// Gets or sets the modality of the <see cref="Section"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Modality"/> value representing the modality of the <see cref="Section"/>,
+        /// or <see langword="null"/> if none was provided.
+        /// </returns>
         public Modality? Mode { get; init; }
+        /// <summary>
+        /// Gets or sets the confidence of the reliability of the <see cref="Mode"/> value, from <c>0.0</c> to <c>1.0</c>.
+        /// </summary>
+        /// <remarks><c>0.0</c> represents low confidence in the value, while <c>1.0</c> represents high confidence.</remarks>
+        /// <returns>A <see cref="Single"/> representing the confidence of the reliability of the <see cref="Mode"/> value.</returns>
         public Single ModeConfidence { get; init; }
+        /// <summary>
+        /// Gets or sets the estimated overall time signature of the <see cref="Section"/>.
+        /// </summary>
+        /// <returns>An <see cref="Int32"/> representing the estimated overall time signature of the <see cref="Section"/>.</returns>
         public Int32 TimeSignature { get; init; }
+        /// <summary>
+        /// Gets or sets the confidence of the reliability of the <see cref="TimeSignature"/> value, from <c>0.0</c> to <c>1.0</c>.
+        /// </summary>
+        /// <remarks><c>0.0</c> represents low confidence in the value, while <c>1.0</c> represents high confidence.</remarks>
+        /// <returns>A <see cref="Single"/> representing the confidence of the reliability of the <see cref="TimeSignature"/> value.</returns>
         public Single TimeSignatureConfidence { get; init; }
     }
 }
--- Spotify.ObjectModel/Segment.cs
+++ Spotify.ObjectModel/Segment.cs
@@ -3,8 +3,30 @@ using System.Collections.Generic;
 
 namespace Spotify.ObjectModel
 {
+    /// <summary>
+    /// Represents a segment of an <see cref="AudioAnalysis"/> object.
+    /// A segment is a subdivision of a <see cref="Track"/> that contains a roughly consistent sound throughout its duration.
+    /// </summary>
     public record Segment : TimeInterval
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Segment"/> record with the specified values.
+        /// </summary>
+        /// <param name="start">A <see cref="Single"/> representing the starting point (in seconds) of the segment.</param>
+        /// <param name="duration">A <see cref="Single"/> representing the duration (in seconds) of the segment.</param>
+        /// <param name="confidence">A <see cref="Single"/> representing the confidence of the reliability of the segmentation.</param>
+        /// <param name="loudnessStart">A <see cref="Single"/> representing the onset loudness (in decibels) of the segment.</param>
+        /// <param name="loudnessEnd">A <see cref="Single"/> representing the offset loudness (in decibels) of the segment.</param>
+        /// <param name="loudnessMax">A <see cref="Single"/> representing the peak loudness (in decibels) of the segment.</param>
+        /// <param name="loudnessMaxTime">
+        /// A <see cref="Single"/> representing the segment-relative offset of the <paramref name="loudnessMax"/> value (in seconds).
+        /// </param>
+        /// <param name="pitches">
+        /// A <see cref="IReadOnlyList{T}"/> of <see cref="Single"/> values representing the pitch content of the segment.
+        /// </param>
+        /// <param name="timbre">
+        /// A <see cref="IReadOnlyList{T}"/> of <see cref="Single"/> values representing the timbre of the segment.
+        /// </param>
         public Segment(
             Single start,
             Single duration,
@@ -25,11 +47,68 @@ namespace Spotify.ObjectModel
             this.Timbre = timbre;
         }
 
+        /// <summary>
+        /// Gets or sets the onset loudness (in decibels) of the <see cref="Segment"/>.
+        /// </summary>
+        /// <remarks>
+        /// Combined with <see cref="LoudnessMax"/> and <see cref="LoudnessMaxTime"/>,
+        /// these components can be used to describe the 'attack' of the <see cref="Segment"/>.
+        /// </remarks>
+        /// <returns>A <see cref="Single"/> representing the onset loudness (in decibels) of the <see cref="Segment"/>.</returns>
         public Single LoudnessStart { get; init; }
+        /// <summary>
+        /// Gets or sets the offset loudness (in decibels) of the <see cref="Segment"/>.
+        /// </summary>
+        /// <remarks>
+        /// This value should be equivalent to the <see cref="LoudnessStart"/> value of the following <see cref="Segment"/>.
+        /// </remarks>
+        /// <returns>A <see cref="Single"/> representing the offset loudness (in decibels) of the segment.</returns>
         public Single LoudnessEnd { get; init; }
+        /// <summary>
+        /// Gets or sets the peak loudness (in decibels) of the <see cref="Segment"/>.
+        /// </summary>
+        /// <remarks>
+        /// Combined with <see cref="LoudnessStart"/> and <see cref="LoudnessMaxTime"/>,
+        /// these components can be used to describe the 'attack' of the <see cref="Segment"/>.
+        /// </remarks>
+        /// <returns>A <see cref="Single"/> representing the peak loudness (in decibels) of the segment.</returns>
         public Single LoudnessMax { get; init; }
+        /// <summary>
+        /// Gets or sets the segment-relative offset of the <see cref="LoudnessMax"/> value (in seconds).
+        /// </summary>
+        /// <remarks>
+        /// Combined with <see cref="LoudnessStart"/> and <see cref="LoudnessMax"/>,
+        /// these components can be used to describe the 'attack' of the <see cref="Segment"/>.
+        /// </remarks>
+        /// <returns>
+        /// A <see cref="Single"/> representing the segment-relative offset of the <see cref="LoudnessMax"/> value (in seconds).
+        /// </returns>
         public Single LoudnessMaxTime { get; init; }
+        /// <summary>
+        /// Gets or sets the pitch values of the <see cref="Segment"/>.
+        /// </summary>
+        /// <remarks>
+        /// The pitch values take the form of a 'chroma' vector corresponding to the 12 pitch classes C, C#, D to B,
+        /// with values ranging from <c>0.0</c> to <c>1.0</c> that describe the relative dominance of every pitch in the chromatic scale.
+        /// More details about how to interpret the vector can be found on the
+        /// <see href="https://spotify.dev/documentation/web-api/reference/tracks/get-audio-analysis#pitch">Spotify for Developers</see> website.
+        /// </remarks>
+        /// <returns>
+        /// A <see cref="IReadOnlyList{T}"/> of <see cref="Single"/> values representing the pitch content of the <see cref="Segment"/>.
+        /// </returns>
         public IReadOnlyList<Single> Pitches { get; init; }
+        /// <summary>
+        /// Gets or sets the timbre of the <see cref="Segment"/>.
+        /// </summary>
+        /// <remarks>
+        /// Timbre is the quality of a musical note or sound that distinguishes different types of musical instruments, or voices.
+        /// The timbre value takes the form of a timbre vector, which is best used in comparison with other timbre vectors.
+        /// More details about how to interpret the vector can be found on the
+        /// <see href="https://spotify.dev/documentation/web-api/reference/tracks/get-audio-analysis#timbre">Spotify for Developers</see> website.
+        /// </remarks>
+        /// <returns>
+        /// A <see cref="IReadOnlyList{T}"/> of <see cref="Single"/> values representing the timbre of the <see cref="Segment"/>.
+        /// </returns>
         public IReadOnlyList<Single> Timbre { get; init; }
     }
 }
