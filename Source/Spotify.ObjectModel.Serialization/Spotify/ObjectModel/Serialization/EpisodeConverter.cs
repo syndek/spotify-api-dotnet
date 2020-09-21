ddea312e88e9dcdd57d196222b677ab5e2b343c4
Louis Salkeld <louissalkeld@outlook.com>
2020-09-21 23:59:56 +0100

    Implement JSON write conversion for Episode

--- Source/Spotify.ObjectModel.Serialization/Spotify/ObjectModel/Serialization/EpisodeConverter.cs
+++ Source/Spotify.ObjectModel.Serialization/Spotify/ObjectModel/Serialization/EpisodeConverter.cs
@@ -140,6 +140,56 @@ namespace Spotify.ObjectModel.Serialization
                 resumePoint);
         }
 
-        public override void Write(Utf8JsonWriter writer, Episode value, JsonSerializerOptions options) => throw new NotSupportedException();
+        public override void Write(Utf8JsonWriter writer, Episode value, JsonSerializerOptions options)
+        {
+            var externalUrlsConverter = options.GetConverter<ExternalUrls>();
+            var imageArrayConverter = options.GetConverter<ImageArray>();
+            var resumePointConverter = options.GetConverter<ResumePoint>();
+            var simplifiedShowConverter = options.GetConverter<SimplifiedShow>();
+            var stringArrayConverter = options.GetConverter<StringArray>();
+            var uriConverter = options.GetConverter<Uri>();
+
+            writer.WriteStartObject();
+            writer.WriteString("id", value.Id);
+            writer.WritePropertyName("uri");
+            uriConverter.Write(writer, value.Uri, options);
+            writer.WritePropertyName("href");
+            uriConverter.Write(writer, value.Href, options);
+            writer.WriteString("name", value.Name);
+            writer.WriteString("description", value.Description);
+            writer.WritePropertyName("images");
+            imageArrayConverter.Write(writer, value.Images, options);
+            writer.WritePropertyName("show");
+            simplifiedShowConverter.Write(writer, value.Show, options);
+            writer.WriteNumber("duration", value.Duration);
+            writer.WriteReleaseDate(value.ReleaseDate, value.ReleaseDatePrecision);
+            writer.WriteString("release_date_precision", value.ReleaseDatePrecision.ToSpotifyString());
+            writer.WriteBoolean("explicit", value.IsExplicit);
+            writer.WriteBoolean("is_playable", value.IsPlayable);
+            writer.WriteBoolean("is_externally_hosted", value.IsExternallyHosted);
+            writer.WritePropertyName("languages");
+            stringArrayConverter.Write(writer, value.Languages, options);
+            if (value.AudioPreviewUrl is Uri audioPreviewUrl)
+            {
+                writer.WritePropertyName("audio_preview_url");
+                uriConverter.Write(writer, audioPreviewUrl, options);
+            }
+            else
+            {
+                writer.WriteNull("audio_preview_url");
+            }
+            writer.WritePropertyName("external_urls");
+            externalUrlsConverter.Write(writer, value.ExternalUrls, options);
+            if (value.ResumePoint is ResumePoint resumePoint)
+            {
+                writer.WritePropertyName("resume_point");
+                resumePointConverter.Write(writer, resumePoint, options);
+            }
+            else
+            {
+                writer.WriteNull("resume_point");
+            }
+            writer.WriteEndObject();
+        }
     }
 }
