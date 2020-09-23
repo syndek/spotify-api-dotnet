e8e852c79594b69cd3cbc568ef5acfe30d3da211
Louis Salkeld <louissalkeld@outlook.com>
2020-09-23 20:55:24 +0100

    Document Recommendations

--- Source/Spotify.ObjectModel/Spotify/ObjectModel/Recommendations.cs
+++ Source/Spotify.ObjectModel/Spotify/ObjectModel/Recommendations.cs
@@ -2,15 +2,44 @@
 
 namespace Spotify.ObjectModel
 {
+    /// <summary>
+    /// Represents a set of recommended tracks.
+    /// </summary>
     public record Recommendations : SpotifyObject
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Recommendations"/> record
+        /// with the specified <paramref name="seeds"/> and <paramref name="tracks"/>.
+        /// </summary>
+        /// <param name="seeds">
+        /// A <see cref="IReadOnlyList{T}"/> of <see cref="RecommendationSeed"/> objects
+        /// representing the seeds of the recommendations.
+        /// </param>
+        /// <param name="tracks">
+        /// A <see cref="IReadOnlyList{T}"/> of <see cref="SimplifiedTrack"/> objects
+        /// representing the recommended tracks of the recommendations.
+        /// </param>
         public Recommendations(IReadOnlyList<RecommendationSeed> seeds, IReadOnlyList<SimplifiedTrack> tracks) : base()
         {
             this.Seeds = seeds;
             this.Tracks = tracks;
         }
 
+        /// <summary>
+        /// Gets or sets the seeds of the <see cref="Recommendations"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="IReadOnlyList{T}"/> of <see cref="RecommendationSeed"/> objects
+        /// representing the seeds of the <see cref="Recommendations"/>.
+        /// </returns>
         public IReadOnlyList<RecommendationSeed> Seeds { get; init; }
+        /// <summary>
+        /// Gets or sets the recommended tracks of the <see cref="Recommendations"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="IReadOnlyList{T}"/> of <see cref="SimplifiedTrack"/> objects
+        /// representing the recommended tracks of the <see cref="Recommendations"/>.
+        /// </returns>
         public IReadOnlyList<SimplifiedTrack> Tracks { get; init; }
     }
 }
