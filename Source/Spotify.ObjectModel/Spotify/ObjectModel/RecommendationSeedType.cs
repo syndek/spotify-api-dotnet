113a1af3c7c8aea3811b2077fda67b29a68f05ec
Louis Salkeld <louissalkeld@outlook.com>
2020-09-23 23:08:26 +0100

    Document RecommendationSeedType

--- Source/Spotify.ObjectModel/Spotify/ObjectModel/RecommendationSeedType.cs
+++ Source/Spotify.ObjectModel/Spotify/ObjectModel/RecommendationSeedType.cs
@@ -2,10 +2,22 @@
 
 namespace Spotify.ObjectModel
 {
+    /// <summary>
+    /// Defines types of <see cref="RecommendationSeed"/>.
+    /// </summary>
     public enum RecommendationSeedType : Int32
     {
+        /// <summary>
+        /// The <see cref="RecommendationSeed"/> is an artist seed.
+        /// </summary>
         Artist,
+        /// <summary>
+        /// The <see cref="RecommendationSeed"/> is a track seed.
+        /// </summary>
         Track,
+        /// <summary>
+        /// The <see cref="RecommendationSeed"/> is a genre seed.
+        /// </summary>
         Genre
     }
 }
