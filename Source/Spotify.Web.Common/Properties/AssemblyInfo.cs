825ef0c84daee70c0fc4760aa052edcc81467451
Louis Salkeld <louissalkeld@outlook.com>
2020-10-09 03:37:25 +0100

    Rename folders that weren't renamed earlier

--- Source/Spotify.Web.Common/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Spotify.Web.Requests")]
-[assembly: InternalsVisibleTo("Spotify.Web.Authorization")]
-[assembly: InternalsVisibleTo("Spotify.Web.Navigation")]
--- Source/Spotify.Web/Properties/AssemblyInfo.cs
+++ Source/Spotify.Web/Properties/AssemblyInfo.cs
@@ -1,3 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Spotify.Navigation")]
+[assembly: InternalsVisibleTo("Spotify.Web.Requests")]
+[assembly: InternalsVisibleTo("Spotify.Web.Authorization")]
+[assembly: InternalsVisibleTo("Spotify.Web.Navigation")]
