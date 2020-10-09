17fe673114894f96d143ba5b7afdb07e1a09f7cc
Louis Salkeld <louissalkeld@outlook.com>
2020-10-09 20:41:02 +0100

    Implement PagingConverterFactory

---
+++ Source/Spotify.ObjectModel.Serialization/Spotify/ObjectModel/Serialization/PagingConverterFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Spotify.ObjectModel.Serialization
+{
+    public class PagingConverterFactory : JsonConverterFactory
+    {
+        public override Boolean CanConvert(Type typeToConvert) =>
+            typeToConvert.IsGenericType && typeToConvert.GetGenericTypeDefinition() == typeof(Paging<>);
+
+        public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (!this.CanConvert(typeToConvert))
+            {
+                throw new ArgumentException($"{typeToConvert} must be a generic Paging type.", nameof(typeToConvert));
+            }
+
+            var elementType = typeToConvert.GetGenericArguments()[0];
+
+            return (JsonConverter) Activator.CreateInstance(
+                typeof(Paging<>).MakeGenericType(new[] { elementType }),
+                BindingFlags.Instance | BindingFlags.Public,
+                binder: null,
+                args: null,
+                culture: null)!;
+        }
+    }
+}
