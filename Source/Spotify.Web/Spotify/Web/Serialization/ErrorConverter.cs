cfcf626d01f938a37557d44428af8a1bae2905e8
Louis Salkeld <louissalkeld@outlook.com>
2020-10-10 04:44:21 +0100

    Implement ErrorConverter

---
+++ Source/Spotify.Web/Spotify/Web/Serialization/ErrorConverter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Spotify.Web.Serialization
+{
+    internal class ErrorConverter : JsonConverter<Error>
+    {
+        public override Error Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType is not JsonTokenType.StartObject)
+            {
+                throw new JsonException();
+            }
+
+            Int32 status = default;
+            String message = String.Empty;
+
+            reader.Read(); // "error"
+            reader.Read(); // {
+
+            while (reader.Read())
+            {
+                if (reader.TokenType is JsonTokenType.EndObject)
+                {
+                    break;
+                }
+
+                if (reader.TokenType is not JsonTokenType.PropertyName)
+                {
+                    throw new JsonException();
+                }
+
+                var propertyName = reader.GetString();
+
+                reader.Read(); // Read to next token.
+
+                switch (propertyName)
+                {
+                    case "status":
+                        status = reader.GetInt32();
+                        break;
+                    case "message":
+                        message = reader.GetString()!;
+                        break;
+                    default:
+                        reader.Skip();
+                        break;
+                }
+            }
+
+            reader.Read(); // }
+
+            return new((HttpStatusCode) status, message);
+        }
+
+        public override void Write(Utf8JsonWriter writer, Error value, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+
+            writer.WritePropertyName("error");
+            writer.WriteStartObject();
+
+            writer.WriteNumber("status", (Int32) value.StatusCode);
+            writer.WriteString("message", value.Message);
+
+            writer.WriteEndObject();
+
+            writer.WriteEndObject();
+        }
+    }
+}
