0b7d57f5ea347420996e1088366b0c3c78b1b0de
Louis Salkeld <louissalkeld@outlook.com>
2020-10-10 04:50:38 +0100

    Implement AuthenticationErrorConverter

---
+++ Source/Spotify.Web.Authorization/Spotify/Web/Authorization/Serialization/AuthenticationErrorConverter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Spotify.Web.Authorization.Serialization
+{
+    internal class AuthenticationErrorConverter : JsonConverter<AuthenticationError>
+    {
+        public override AuthenticationError Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType is not JsonTokenType.StartObject)
+            {
+                throw new JsonException();
+            }
+
+            String error = String.Empty;
+            String? errorDescription = null;
+
+            while (reader.Read())
+            {
+                if (reader.TokenType is JsonTokenType.EndObject)
+                {
+                    break;
+                }
+
+                if (reader.TokenType is not JsonTokenType.PropertyName)
+                {
+                    throw new JsonException();
+                }
+
+                var propertyName = reader.GetString();
+
+                reader.Read(); // Read to next token.
+
+                switch (propertyName)
+                {
+                    case "error":
+                        error = reader.GetString()!;
+                        break;
+                    case "error_description":
+                        errorDescription = reader.GetString();
+                        break;
+                    default:
+                        reader.Skip();
+                        break;
+                }
+            }
+
+            return new(error, errorDescription);
+        }
+
+        public override void Write(Utf8JsonWriter writer, AuthenticationError value, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+
+            writer.WriteString("error", value.Error);
+            writer.WriteString("error_description", value.ErrorDescription);
+
+            writer.WriteEndObject();
+        }
+    }
+}
