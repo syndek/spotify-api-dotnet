af9a76398ec1bed171d20cac11a57fb76c539993
Louis Salkeld <louissalkeld@outlook.com>
2020-10-12 05:45:04 +0100

    Add tests for AlbumConverter

--- Tests/Spotify.ObjectModel.Serialization.Tests/Resources/TestData.Designer.cs
+++ Tests/Spotify.ObjectModel.Serialization.Tests/Resources/TestData.Designer.cs
@@ -61,6 +61,15 @@ namespace Spotify.ObjectModel.Serialization.Tests.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to {&quot;album_type&quot;:&quot;album&quot;,&quot;artists&quot;:[{&quot;external_urls&quot;:{&quot;spotify&quot;:&quot;https://open.spotify.com/artist/4pb4rqWSoGUgxm63xmJ8xc&quot;},&quot;href&quot;:&quot;https://api.spotify.com/v1/artists/4pb4rqWSoGUgxm63xmJ8xc&quot;,&quot;id&quot;:&quot;4pb4rqWSoGUgxm63xmJ8xc&quot;,&quot;name&quot;:&quot;Madeon&quot;,&quot;type&quot;:&quot;artist&quot;,&quot;uri&quot;:&quot;spotify:artist:4pb4rqWSoGUgxm63xmJ8xc&quot;}],&quot;available_markets&quot;:[&quot;AD&quot;,&quot;AE&quot;,&quot;AL&quot;,&quot;AR&quot;,&quot;AT&quot;,&quot;AU&quot;,&quot;BA&quot;,&quot;BE&quot;,&quot;BG&quot;,&quot;BH&quot;,&quot;BO&quot;,&quot;BR&quot;,&quot;BY&quot;,&quot;CA&quot;,&quot;CH&quot;,&quot;CL&quot;,&quot;CO&quot;,&quot;CR&quot;,&quot;CY&quot;,&quot;CZ&quot;,&quot;DE&quot;,&quot;DK&quot;,&quot;DO&quot;,&quot;DZ&quot;,&quot;EC&quot;,&quot;EE&quot;,&quot;EG&quot;,&quot;ES&quot;,&quot;FI&quot;,&quot;FR&quot;,&quot;GB&quot;,&quot;GR&quot;,&quot;GT&quot;,&quot;HK&quot;,&quot;HN&quot;,&quot;HR&....
+        /// </summary>
+        internal static string AlbumJson {
+            get {
+                return ResourceManager.GetString("AlbumJson", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to {&quot;external_urls&quot;:{&quot;spotify&quot;:&quot;https://open.spotify.com/artist/4pb4rqWSoGUgxm63xmJ8xc&quot;},&quot;followers&quot;:{&quot;href&quot;:null,&quot;total&quot;:542633},&quot;genres&quot;:[&quot;big room&quot;,&quot;complextro&quot;,&quot;edm&quot;,&quot;electro house&quot;,&quot;electropop&quot;,&quot;filter house&quot;,&quot;future bass&quot;,&quot;nantes indie&quot;],&quot;href&quot;:&quot;https://api.spotify.com/v1/artists/4pb4rqWSoGUgxm63xmJ8xc&quot;,&quot;id&quot;:&quot;4pb4rqWSoGUgxm63xmJ8xc&quot;,&quot;images&quot;:[{&quot;height&quot;:640,&quot;url&quot;:&quot;https://i.scdn.co/image/426e0d784cfc0dfe8f3275e32850223232835c14&quot;,&quot;width&quot;:640},{&quot;height&quot;:320,&quot;url&quot;:&quot;https://i.scdn.co/image/d12113789d3953523b7 [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string ArtistJson {
