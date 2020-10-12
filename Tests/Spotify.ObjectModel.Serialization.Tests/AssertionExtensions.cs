5ebb5af63745a1b6bf117d4975f6eeec8ae465a4
Louis Salkeld <louissalkeld@outlook.com>
2020-10-12 05:08:09 +0100

    Create AssertionExtensions and clean-up code

---
+++ Tests/Spotify.ObjectModel.Serialization.Tests/AssertionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Spotify.ObjectModel.Serialization.Tests
+{
+    internal static class AssertionExtensions : Object
+    {
+        internal static void PagingsAreEqual<TObject>(this CollectionAssert assert, Paging<TObject> expected, Paging<TObject> actual)
+        {
+            Assert.AreEqual(expected.Total, actual.Total);
+            Assert.AreEqual(expected.Limit, actual.Limit);
+            Assert.AreEqual(expected.Offset, actual.Offset);
+            Assert.AreEqual(expected.Href, actual.Href);
+            Assert.AreEqual(expected.Previous, actual.Previous);
+            Assert.AreEqual(expected.Next, actual.Next);
+            CollectionAssert.AreEqual(expected.ToArray(), actual.ToArray());
+        }
+    }
+}
