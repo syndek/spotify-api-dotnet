d6e9a323ffcf0979e11cba802103cf13d4036c4b
Louis Salkeld <louissalkeld@outlook.com>
2020-10-12 06:45:42 +0100

    Implement ImmutableValueArray and tests
    This new struct can provide structural equality for collections within ObjectModel types.

---
+++ Source/Spotify.ObjectModel/Spotify/ObjectModel/Collections/ImmutableValueArray.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Spotify.ObjectModel.Collections
+{
+    public readonly struct ImmutableValueArray<TElement> : IEquatable<ImmutableValueArray<TElement>>, IReadOnlyList<TElement>
+    {
+        private readonly IReadOnlyList<TElement> elements;
+
+        public ImmutableValueArray(IEnumerable<TElement> elements)
+        {
+            this.elements = elements.ToArray();
+        }
+
+        public TElement this[Int32 index] => this.elements[index];
+
+        public Int32 Count => this.elements.Count;
+
+        public override Boolean Equals(Object? obj) => (obj is ImmutableValueArray<TElement> array) && this.Equals(array);
+
+        public override Int32 GetHashCode()
+        {
+            var hashCode = new HashCode();
+
+            foreach (var element in this)
+            {
+                hashCode.Add(element);
+            }
+
+            return hashCode.ToHashCode();
+        }
+
+        public Boolean Equals(ImmutableValueArray<TElement> other)
+        {
+            if (this.Count != other.Count)
+            {
+                return false;
+            }
+
+            for (var i = 0; i < this.Count; i++)
+            {
+                if (this[i]?.Equals(other[i]) == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public IEnumerator<TElement> GetEnumerator() => this.elements.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => this.elements.GetEnumerator();
+
+        public static Boolean operator ==(ImmutableValueArray<TElement>? left, ImmutableValueArray<TElement>? right) => (left, right) switch
+        {
+            (null, null) => true,
+            (not null, not null) => left.Equals(right),
+            _ => false
+        };
+
+        public static Boolean operator !=(ImmutableValueArray<TElement>? left, ImmutableValueArray<TElement>? right) => !(left == right);
+    }
+}
---
+++ Tests/Spotify.ObjectModel.Tests/ImmutableValueArrayTests.cs
@@ -0,0 +1,49 @@
+using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Spotify.ObjectModel.Collections;
+
+namespace Spotify.ObjectModel.Tests
+{
+    [TestClass]
+    public class ImmutableValueArrayTests : Object
+    {
+        [TestMethod]
+        public void StructuralEqualityWithStructuralEqualityElements()
+        {
+            // These 2 arrays *should not* be equal because they *do not* use structural equality.
+            var array1 = new[] { "A", "B", "C" };
+            var array2 = new[] { "A", "B", "C" };
+
+            Assert.AreNotSame(array1, array2);
+            Assert.AreNotEqual(array1, array2);
+
+            // These 2 arrays *should* be equal because they *do* use structural equality.
+            var valueArray1 = new ImmutableValueArray<String>(array1);
+            var valueArray2 = new ImmutableValueArray<String>(array2);
+
+            Assert.AreNotSame(valueArray1, valueArray2);
+            Assert.AreEqual(valueArray1, valueArray2);
+        }
+
+        [TestMethod]
+        public void StructuralEqualityWithReferenceEqualityElements()
+        {
+            // These 2 arrays *should not* be equal because they *do not* use structural equality.
+            var array1 = new Object[] { new(), new(), new() };
+            var array2 = new Object[] { new(), new(), new() };
+
+            Assert.AreNotSame(array1, array2);
+            Assert.AreNotEqual(array1, array2);
+
+            // These 2 arrays also *should not* be equal.
+            // Even though they use structural equality, the elements inside *do not*.
+            var valueArray1 = new ImmutableValueArray<Object>(array1);
+            var valueArray2 = new ImmutableValueArray<Object>(array2);
+
+            Assert.AreNotSame(valueArray1, valueArray2);
+            Assert.AreNotEqual(valueArray1, valueArray2);
+        }
+    }
+}
