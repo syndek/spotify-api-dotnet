3270031b9d26fc59089e1d1f799b079436fa2ba3
Louis Salkeld <louissalkeld@outlook.com>
2020-10-13 02:48:02 +0100

    Implement ImmutableValueDictionary and tests
    This new struct can provide structural equality for dictionaries within ObjectModel types.

---
+++ Source/Spotify.ObjectModel/Spotify/ObjectModel/Collections/ImmutableValueDictionary.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Spotify.ObjectModel.Collections
+{
+    public readonly struct ImmutableValueDictionary<TKey, TValue> : IEquatable<ImmutableValueDictionary<TKey, TValue>>, IReadOnlyDictionary<TKey, TValue> where TKey : notnull
+    {
+        private readonly IReadOnlyDictionary<TKey, TValue> dictionary;
+
+        public ImmutableValueDictionary(IEnumerable<KeyValuePair<TKey, TValue>> elements)
+        {
+            this.dictionary = new Dictionary<TKey, TValue>(elements);
+        }
+
+        public TValue this[TKey key] => this.dictionary[key];
+
+        public IEnumerable<TKey> Keys => this.dictionary.Keys;
+        public IEnumerable<TValue> Values => this.dictionary.Values;
+        public Int32 Count => this.dictionary.Count;
+
+        public override Boolean Equals(Object? obj) => (obj is ImmutableValueDictionary<TKey, TValue> dictionary) && this.Equals(dictionary);
+
+        // This probably needs to be done better, but will work 'good enough' for now.
+        public override Int32 GetHashCode() => this.dictionary.GetHashCode();
+
+        public Boolean ContainsKey(TKey key) => this.dictionary.ContainsKey(key);
+
+        public Boolean Equals(ImmutableValueDictionary<TKey, TValue> other)
+        {
+            if (this.Count != other.Count)
+            {
+                return false;
+            }
+
+            var valueComparer = EqualityComparer<TValue>.Default;
+
+            foreach (var element in this)
+            {
+                if (!other.TryGetValue(element.Key, out var value))
+                {
+                    return false;
+                }
+
+                if (!valueComparer.Equals(element.Value, value))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => this.dictionary.GetEnumerator();
+
+        public Boolean TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value) => this.dictionary.TryGetValue(key, out value);
+
+        IEnumerator IEnumerable.GetEnumerator() => this.dictionary.GetEnumerator();
+
+        public static Boolean operator ==(ImmutableValueDictionary<TKey, TValue>? left, ImmutableValueDictionary<TKey, TValue>? right) => (left, right) switch
+        {
+            (null, null) => true,
+            (not null, not null) => left.Equals(right),
+            _ => false
+        };
+
+        public static Boolean operator !=(ImmutableValueDictionary<TKey, TValue>? left, ImmutableValueDictionary<TKey, TValue>? right) => !(left == right);
+    }
+}
---
+++ Tests/Spotify.ObjectModel.Tests/ImmutableValueDictionaryTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Spotify.ObjectModel.Collections;
+
+namespace Spotify.ObjectModel.Tests
+{
+    [TestClass]
+    public class ImmutableValueDictionaryTests : Object
+    {
+        [TestMethod]
+        public void StructuralEqualityWithStructuralEqualityElements()
+        {
+            // These 2 dictionaries *should not* be equal because they *do not* use structural equality.
+            var dictionary1 = new Dictionary<String, Int32> { { "A", 1 }, { "B", 2 }, { "C", 3 } };
+            var dictionary2 = new Dictionary<String, Int32> { { "A", 1 }, { "B", 2 }, { "C", 3 } };
+
+            Assert.AreNotSame(dictionary1, dictionary2);
+            Assert.AreNotEqual(dictionary1, dictionary2);
+
+            // These 2 dictionaries *should* be equal because they *do* use structural equality.
+            var valueDictionary1 = new ImmutableValueDictionary<String, Int32>(dictionary1);
+            var valueDictionary2 = new ImmutableValueDictionary<String, Int32>(dictionary2);
+
+            Assert.AreNotSame(valueDictionary1, valueDictionary2);
+            Assert.AreEqual(valueDictionary1, valueDictionary2);
+        }
+
+        [TestMethod]
+        public void StructuralEqualityWithReferenceEqualityElements()
+        {
+            // These 2 dictionaries *should not* be equal because they *do not* use structural equality.
+            var dictionary1 = new Dictionary<Object, Object> { { new(), new() }, { new(), new() }, { new(), new() } };
+            var dictionary2 = new Dictionary<Object, Object> { { new(), new() }, { new(), new() }, { new(), new() } };
+
+            Assert.AreNotSame(dictionary1, dictionary2);
+            Assert.AreNotEqual(dictionary1, dictionary2);
+
+            // These 2 dictionaries also *should not* be equal.
+            // Even though they use structural equality, the elements inside *do not*.
+            var valueDictionary1 = new ImmutableValueDictionary<Object, Object>(dictionary1);
+            var valueDictionary2 = new ImmutableValueDictionary<Object, Object>(dictionary2);
+
+            Assert.AreNotSame(valueDictionary1, valueDictionary2);
+            Assert.AreNotEqual(valueDictionary1, valueDictionary2);
+        }
+
+        [TestMethod]
+        public void StructuralEqualityWithReferenceEqualityKeysAndStructuralEqualityValues()
+        {
+            // These 2 dictionaries *should not* be equal because they *do not* use structural equality.
+            var dictionary1 = new Dictionary<Object, Int32> { { new(), 1 }, { new(), 2 }, { new(), 3 } };
+            var dictionary2 = new Dictionary<Object, Int32> { { new(), 1 }, { new(), 2 }, { new(), 3 } };
+
+            Assert.AreNotSame(dictionary1, dictionary2);
+            Assert.AreNotEqual(dictionary1, dictionary2);
+
+            // These 2 dictionaries also *should not* be equal.
+            // Even though they use structural equality, the keys inside *do not*.
+            var valueArray1 = new ImmutableValueDictionary<Object, Int32>(dictionary1);
+            var valueArray2 = new ImmutableValueDictionary<Object, Int32>(dictionary2);
+
+            Assert.AreNotSame(valueArray1, valueArray2);
+            Assert.AreNotEqual(valueArray1, valueArray2);
+        }
+
+        [TestMethod]
+        public void StructuralEqualityWithStructuralEqualityKeysAndReferenceEqualityValues()
+        {
+            // These 2 dictionaries *should not* be equal because they *do not* use structural equality.
+            var dictionary1 = new Dictionary<String, Object> { { "A", new() }, { "B", new() }, { "C", new() } };
+            var dictionary2 = new Dictionary<String, Object> { { "A", new() }, { "B", new() }, { "C", new() } };
+
+            Assert.AreNotSame(dictionary1, dictionary2);
+            Assert.AreNotEqual(dictionary1, dictionary2);
+
+            // These 2 dictionaries also *should not* be equal.
+            // Even though they use structural equality, the values inside *do not*.
+            var valueArray1 = new ImmutableValueDictionary<String, Object>(dictionary1);
+            var valueArray2 = new ImmutableValueDictionary<String, Object>(dictionary2);
+
+            Assert.AreNotSame(valueArray1, valueArray2);
+            Assert.AreNotEqual(valueArray1, valueArray2);
+        }
+    }
+}
