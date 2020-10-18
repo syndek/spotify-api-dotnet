5052727b64455625d9b168e9ff994c68e0f37775
Louis Salkeld <louissalkeld@outlook.com>
2020-10-18 22:02:11 +0100

    Add tests for SimplifiedTrackConverter

---
+++ Tests/Spotify.ObjectModel.Serialization.Tests/SimplifiedTrackConverterTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Spotify.ObjectModel.Serialization.Tests.Resources;
+
+namespace Spotify.ObjectModel.Serialization.Tests
+{
+    [TestClass]
+    public class SimplifiedTrackConverterTests : JsonConverterTests<SimplifiedTrack>
+    {
+        public override String TestJson => TestData.TrackJson;
+        public override JsonSerializerOptions SerializerOptions => new()
+        {
+            Converters =
+            {
+                new SimplifiedTrackConverter(),
+                new CountryCodeConverter(),
+                new SimplifiedArtistConverter()
+            }
+        };
+    }
+}
