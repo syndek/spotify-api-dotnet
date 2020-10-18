c3f2ef7edf9a078e396de23edb177ccbc911c185
Louis Salkeld <louissalkeld@outlook.com>
2020-10-18 02:25:23 +0100

    Add tests for SimplifiedPlaylistConverter

---
+++ Tests/Spotify.ObjectModel.Serialization.Tests/SimplifiedPlaylistConverterTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.Json;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Spotify.ObjectModel.Serialization.Tests.Resources;
+
+namespace Spotify.ObjectModel.Serialization.Tests
+{
+    [TestClass]
+    public class SimplifiedPlaylistConverterTests : JsonConverterTests<SimplifiedPlaylist>
+    {
+        // Currently, it looks like SimplifiedPlaylist is unused in the Spotify Web API.
+        // As a result, we just use a regular Playlist JSON object to test this converter.
+        // It should be identical to a SimplifiedPlaylist JSON object, with an extra 'followers' field that gets ignored.
+        public override String TestJson => TestData.PlaylistJson;
+        public override JsonSerializerOptions SerializerOptions => new()
+        {
+            Converters =
+            {
+                new SimplifiedPlaylistConverter(),
+                new ImageConverter(),
+                new PagingConverterFactory(),
+                new PlaylistTrackConverter(),
+                new PlayableConverter(),
+                new PublicUserConverter()
+            }
+        };
+    }
+}
