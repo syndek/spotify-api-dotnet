360bdf1fe79b8b1f95e28d902f673b1db736267a
Louis Salkeld <louissalkeld@outlook.com>
2020-12-31 18:53:50 +0000

    Document ISpotifySearchApi/SpotifySearchApiClient

--- Source/Spotify.Abstractions/Spotify/ISpotifySearchApi.cs
+++ Source/Spotify.Abstractions/Spotify/ISpotifySearchApi.cs
@@ -10,6 +10,28 @@ namespace Spotify
     /// </summary>
     public interface ISpotifySearchApi
     {
+        /// <summary>
+        /// Asynchronously search the Spotify catalog using a keyword string.
+        /// </summary>
+        /// <param name="query">
+        /// A <see cref="string"/> representing the keywords to search with.
+        /// This can contain optional field filters and operators. Information about how to use these can be found in
+        /// the <see href="https://spotify.dev/documentation/web-api/reference/search/search/">Spotify Developer documentation</see>.
+        /// </param>
+        /// <param name="types">The <see cref="SearchResultTypes"/> to include in the search.</param>
+        /// <param name="market">
+        /// An optional <see cref="CountryCode"/> to limit returned content to a certain market.
+        /// Playlists are not affected by this parameter.
+        /// <see cref="CountryCode.FromToken"/> can be used if the access token used is a valid user access token.
+        /// </param>
+        /// <param name="limit">
+        /// The maximum number of results to return.
+        /// This limit is applied within each type, not on the total response.
+        /// </param>
+        /// <param name="offset">The index of the first result to return.</param>
+        /// <param name="includeExternal">Whether or not to include any relevant audio content that is hosted externally.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for cancellation requests.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the asynchronous operation.</returns>
         Task<SearchResult> SearchAsync(
             string query,
             SearchResultTypes types,
--- Source/Spotify.Web.Requests/Spotify/Web/SpotifySearchApiClient.cs
+++ Source/Spotify.Web.Requests/Spotify/Web/SpotifySearchApiClient.cs
@@ -21,6 +21,29 @@ namespace Spotify.Web
         /// <param name="httpClient">An instance of <see cref="HttpClient"/> to use for requests to the Spotify Web API.</param>
         public SpotifySearchApiClient(HttpClient httpClient) : base(httpClient) { }
 
+        /// <summary>
+        /// Asynchronously search the Spotify catalog using a keyword string.
+        /// </summary>
+        /// <param name="query">
+        /// A <see cref="string"/> representing the keywords to search with.
+        /// This can contain optional field filters and operators. Information about how to use these can be found in
+        /// the <see href="https://spotify.dev/documentation/web-api/reference/search/search/">Spotify Developer documentation</see>.
+        /// </param>
+        /// <param name="types">The <see cref="SearchResultTypes"/> to include in the search.</param>
+        /// <param name="market">
+        /// An optional <see cref="CountryCode"/> to limit returned content to a certain market.
+        /// Playlists are not affected by this parameter.
+        /// <see cref="CountryCode.FromToken"/> can be used if the access token used is a valid user access token.
+        /// </param>
+        /// <param name="limit">
+        /// The maximum number of results to return.
+        /// This limit is applied within each type, not on the total response.
+        /// </param>
+        /// <param name="offset">The index of the first result to return.</param>
+        /// <param name="includeExternal">Whether or not to include any relevant audio content that is hosted externally.</param>
+        /// <param name="accessTokenProvider">An optional <see cref="IAccessTokenProvider"/> to use instead of the default.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for cancellation requests.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the asynchronous operation.</returns>
         public Task<SearchResult> SearchAsync(
             string query,
             SearchResultTypes types,
@@ -47,6 +70,7 @@ namespace Spotify.Web
                 cancellationToken);
         }
 
+        #region ISpotifySearchApi Implementation
         Task<SearchResult> ISpotifySearchApi.SearchAsync(
             string query,
             SearchResultTypes types,
@@ -58,5 +82,6 @@ namespace Spotify.Web
         {
             return SearchAsync(query, types, market, limit, offset, includeExternal, null, cancellationToken);
         }
+        #endregion
     }
 }
